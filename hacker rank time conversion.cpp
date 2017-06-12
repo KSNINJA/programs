1f7c8c5c649a5ef0720357fcbecb80a8e6778ea4
unknown <kartiksinghal122@gmail.com>
2017-06-12 11:03:06 +0530

    first commit

--- hacker rank time conversion.cpp
+++ hacker rank time conversion.cpp
@@ -5,12 +5,27 @@ int main()
 {
     string TwelveHourSystem;
     cin>>TwelveHourSystem;
-    int HoursInTwentyFourHourSystem=12+(((int) TwelveHourSystem[0])-48)*10+((int)TwelveHourSystem[1])-48;
-    cout<<HoursInTwentyFourHourSystem;
-    int i=2;
-    while(i<8)
+    if(TwelveHourSystem[8]=='p')
     {
-        cout<<TwelveHourSystem[i];
-        i++;
+    int HoursInTwentyFourHourSystem=12+(((int) TwelveHourSystem[0])-48)*10+((int)TwelveHourSystem[1])-48;
+        cout<<HoursInTwentyFourHourSystem;
+       int i=2;
+        while(i<8)
+        {
+            cout<<TwelveHourSystem[i];
+            i++;
+        }
+
     }
+     else
+     {
+         int i=0;
+         while(i<8)
+         {
+             cout<<TwelveHourSystem[i];
+             i++;
+         }
+     }
+
+
 }
