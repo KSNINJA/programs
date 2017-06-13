a5be0ce6849c021577da2c6b074a6228f1c62c79
KS NINJA <kartiksinghal122@gmail.com>
2017-06-13 15:22:15 +0530

    Update hacker rank time conversion.cpp

--- hacker rank time conversion.cpp
+++ hacker rank time conversion.cpp
@@ -5,27 +5,44 @@ int main()
 {
     string TwelveHourSystem;
     cin>>TwelveHourSystem;
-    if(TwelveHourSystem[8]=='p')
-    {
-    int HoursInTwentyFourHourSystem=12+(((int) TwelveHourSystem[0])-48)*10+((int)TwelveHourSystem[1])-48;
-        cout<<HoursInTwentyFourHourSystem;
-       int i=2;
-        while(i<8)
+    int a=(((int) TwelveHourSystem[0])-48)*10+((int)TwelveHourSystem[1]-48);
+    if(a>=1&&TwelveHourSystem[8]=='A')
         {
+        int i=0;
+        while(i<8)
+            {
             cout<<TwelveHourSystem[i];
             i++;
         }
-
     }
-     else
-     {
-         int i=0;
-         while(i<8)
-         {
-             cout<<TwelveHourSystem[i];
-             i++;
-         }
-     }
-
-
-}
+       else if(a>=1&&TwelveHourSystem[8]=='P')
+            {
+            cout<<a+12;
+            int i=2;
+            while(i<8)
+                {
+                cout<<TwelveHourSystem[i];
+                i++;
+            }
+        }
+        else if(a==12&&TwelveHourSystem[8]=='A')
+            {
+            cout<<"00";
+            int i=2;
+            while(i<8)
+                {
+                cout<<TwelveHourSystem[i];
+                i++;
+            }
+        }
+            else
+                {
+                cout<<12;
+                int i=2;
+                while(i<8)
+                    {
+                    cout<<TwelveHourSystem[i];
+                    i++;
+                }
+            }
+        }
